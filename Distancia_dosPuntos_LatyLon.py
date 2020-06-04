16142ac7a5950aa00440884be66d345b9a95735b
Francisco Palm <fpalm@qu4nt.com>
2020-06-04 19:05:38 -0400

    Convertir llamadas a funciones trignométricas

--- Distancia_dosPuntos_LatyLon.py
+++ Distancia_dosPuntos_LatyLon.py
@@ -2,19 +2,19 @@
 
 Calcular la distancia entre dos puntos
 """
-import math
+from math import radians, acos, sin, cos
 #%%#Introduciendo las coordenadas y convirtiendolas de Grados decimales a Radianes
 #Punto inicial
-latini = math.radians(float(input("Introduzca la Latitud inicial: ")))
+latini = radians(float(input("Introduzca la Latitud inicial: ")))
 print (latini)
-lonini = math.radians(float(input("Introduce la Longitud inicial: ")))
+lonini = radians(float(input("Introduce la Longitud inicial: ")))
 print (lonini)
 #Punto final
-latfin = math.radians(float(input("Introduzca la Latitud final: ")))
+latfin = radians(float(input("Introduzca la Latitud final: ")))
 print (latfin)
-lonfin = math.radians(float(input("Introduce La Longitud final: ")))
+lonfin = radians(float(input("Introduce La Longitud final: ")))
 print (lonfin)
 #%%#Formula para calcular la distancia
 r = 6371.01
-distancia = r*(math.acos(math.sin(latini)*math.sin(latfin)+math.cos(latini)*math.cos(latfin)*math.cos(lonini-lonfin)))
+distancia = r*(acos(sin(latini)*sin(latfin)+cos(latini)*cos(latfin)*cos(lonini-lonfin)))
 print ("La distancia entre ambos puntos en Km es de: ",distancia)
