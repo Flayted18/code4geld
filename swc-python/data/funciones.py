74c1e287b6f21a569c206d9bdda5996239180bbe
Santiago Faysal <laultimadesantiago@gmail.com>
2020-06-19 16:43:47 -0400

    Correcciones

--- swc-python/data/funciones.py
+++ swc-python/data/funciones.py
@@ -8,17 +8,17 @@ filenames = filenames[0:3]
 def visualize(files):
         data = np.loadtxt(fname=files, delimiter=",")
         
-        fig = plt.figure(figsize = 10.0, 3.0)
+        fig = plt.figure(figsize = (10.0, 3.0))
         
         axes1 = fig.add_subplot(1, 3, 1)
         axes2 = fig.add_subplot(1, 3, 2)
         axes3 = fig.add_subplot(1, 3, 3)
         
-        axes1 = set_ylabel('average')
+        axes1.set_ylabel('average')
         axes1.plot(np.mean(data, axis = 0))
-        axes2 = set_ylabel('max')
+        axes2.set_ylabel('max')
         axes2.plot(np.max(data, axis = 0))
-        axes3 = set_ylabel('min')
-        axes.plot(np.min(data, axis = 0))
+        axes3.set_ylabel('min')
+        axes3.plot(np.min(data, axis = 0))
         
         fig.tight_layout()
